1abc7b7b962f1d120f0e0bc781998fbd9d07dd81
Guy John <guy@rumblesan.com>
2016-01-13 17:36:30 +0000

    Updating ast

--- main/include/ast.h
+++ main/include/ast.h
@@ -3,58 +3,113 @@
 
 #include "list.h"
 
-typedef enum {APPLICATION, VARDEF, NUMBER, VARIABLE} ASTNodeTypes;
-
 /* Pointers */
-typedef struct astNode           *AST;
+typedef struct blockNode         *Block;
+typedef struct elementNode       *Element;
 typedef struct applicationNode   *Application;
+typedef struct argListNode       *ArgList;
 typedef struct varDefinitionNode *VarDefinition;
+typedef struct expressionNode    *Expression;
 typedef struct numberNode        *Number;
 typedef struct variableNode      *Variable;
+typedef char                     *Identifier;
 
-/* General AST Node */
-typedef struct astNode {
+/* Block AST Node */
+typedef struct blockNode {
 
-    ASTNodeTypes nodeType;
+    List elements;
+
+} BlockNode;
+
+Block ast_block_create(List nodes);
+
+void ast_block_cleanup(Block block);
+
+/* Element AST Node */
+typedef enum {VARDEFINITIONEL, APPLICATIONEL} ElementNodeType;
+
+typedef struct elementNode {
+
+    ElementNodeType elementType;
 
     union {
         Application application;
         VarDefinition varDefinition;
-        Number number;
-        Variable variable;
     };
 
-} ASTNode;
+} ElementNode;
 
-AST ast_create();
+Element ast_element_create();
 
-void ast_cleanup(AST ast);
+void ast_element_cleanup(Element element);
+
+Element ast_application_element(Application application);
+
+Element ast_vardefinition_element(VarDefinition vardefinition);
+
 
 /* Application AST Node */
 typedef struct applicationNode {
 
     char *name;
 
-    List args;
+    ArgList args;
 
 } ApplicationNode;
 
-AST ast_application_create(char *name, List args);
+Application ast_application_create(char *name, ArgList args);
 
 void ast_application_cleanup(Application application);
+
+/* ArgList AST Node */
+typedef struct argListNode {
+
+    List args;
+
+} ArgListNode;
+
+ArgList ast_arg_list_create(List args);
+
+void ast_arg_list_cleanup(List args);
 
 /* Variable Definition AST Node */
 typedef struct varDefinitionNode {
 
     char *name;
 
-    AST expression;
+    Expression expression;
 
 } VarDefinitionNode;
 
-AST ast_vardef_create(char *name, AST expression);
+VarDefinition ast_vardef_create(char *name, Expression expression);
 
 void ast_vardef_cleanup(VarDefinition vardef);
+
+
+/* Expression AST Node */
+typedef enum {APPLICATIONEXPR, NUMBEREXPR, VARIABLEEXPR} ExpressionNodeType;
+
+typedef struct expressionNode {
+
+    ExpressionNodeType expressionType;
+
+    union {
+        Application application;
+        Number number;
+        Variable variable;
+    };
+
+} ExpressionNode;
+
+Expression ast_expression_create();
+
+void ast_expression_cleanup(Expression expression);
+
+Expression ast_application_expression(Application application);
+
+Expression ast_number_expression(Number number);
+
+Expression ast_variable_expression(Variable variable);
 
 /* Number Literal AST Node */
 typedef struct numberNode {
@@ -63,7 +118,7 @@ typedef struct numberNode {
 
 } NumberNode;
 
-AST ast_number_create(double value);
+Number ast_number_create(double value);
 
 void ast_number_cleanup(Number number);
 
@@ -74,7 +129,7 @@ typedef struct variableNode {
 
 } VariableNode;
 
-AST ast_variable_create(char *name);
+Variable ast_variable_create(char *name);
 
 void ast_number_cleanup(Number number);
 
--- main/src/ast.c
+++ main/src/ast.c
@@ -3,62 +3,124 @@
 #include "ast.h"
 #include "list.h"
 
-/* General AST Node */
-AST ast_create() {
-    AST ast = malloc(sizeof(ASTNode));
-    return ast;
+/* Block AST Node */
+Block ast_block_create(List elements) {
+    Block block = malloc(sizeof(BlockNode));
+    block->elements = elements;
+
+    return block;
 }
 
-void ast_cleanup(AST ast) {
-    // FIXME Implement
+void ast_block_cleanup(Block block) {
+    // TODO Free up nodes
+    free(block);
+}
+
+/* Element AST Node */
+Element ast_element_create() {
+    Element element = malloc(sizeof(ElementNode));
+
+    return element;
+}
+
+void ast_element_cleanup(Element element) {
+    // TODO Free up node
+    free(element);
+}
+
+Element ast_application_element(Application application) {
+    Element element = ast_element_create();
+    element->elementType = APPLICATIONEL;
+    element->application = application;
+    return element;
+}
+
+Element ast_vardefinition_element(VarDefinition varDefinition) {
+    Element element = ast_element_create();
+    element->elementType = VARDEFINITIONEL;
+    element->varDefinition = varDefinition;
+    return element;
 }
 
 /* Application AST Node */
-AST ast_application_create(char *name, List args) {
+Application ast_application_create(char *name, ArgList args) {
     Application application = malloc(sizeof(ApplicationNode));
     application->name = name;
     application->args = args;
 
-    AST ast = ast_create();
-    ast->nodeType = APPLICATION;
-    ast->application = application;
-
-    return ast;
+    return application;
 }
 
 void ast_application_cleanup(Application application) {
-    // TODO Free up name and args as well?
+    // TODO Free up name and args
     free(application);
 }
 
+/* ArgList AST Node */
+ArgList ast_arg_list_create(List args) {
+    ArgList argList = malloc(sizeof(ArgListNode));
+    argList->args = args;
+
+    return argList;
+}
+
+void ast_arg_list_cleanup(List argList) {
+    // TODO Free up args
+    free(argList);
+}
+
 /* Variable Definition AST Node */
-AST ast_vardef_create(char *name, AST expression) {
+VarDefinition ast_vardef_create(char *name, Expression expression) {
     VarDefinition varDef = malloc(sizeof(VarDefinitionNode));
     varDef->name = name;
     varDef->expression = expression;
 
-    AST ast = ast_create();
-    ast->nodeType = VARDEF;
-    ast->varDefinition = varDef;
-
-    return ast;
+    return varDef;
 }
 
 void ast_vardef_cleanup(VarDefinition vardef) {
-    // TODO Free up name and expr as well?
+    // TODO Free up name and expr
     free(vardef);
 }
 
+/* Expression AST Node */
+Expression ast_expression_create() {
+    Expression expression = malloc(sizeof(ExpressionNode));
+    return expression;
+}
+
+void ast_expression_cleanup(Expression expression) {
+    // TODO Free up expr
+    free(expression);
+}
+
+Expression ast_application_expression(Application application) {
+    Expression expression = ast_expression_create();
+    expression->expressionType = APPLICATIONEXPR;
+    expression->application = application;
+    return expression;
+}
+
+Expression ast_number_expression(Number number) {
+    Expression expression = ast_expression_create();
+    expression->expressionType = NUMBEREXPR;
+    expression->number = number;
+    return expression;
+}
+
+Expression ast_variable_expression(Variable variable) {
+    Expression expression = ast_expression_create();
+    expression->expressionType = VARIABLEEXPR;
+    expression->variable = variable;
+    return expression;
+}
+
 /* Number Literal AST Node */
-AST ast_number_create(double value) {
+Number ast_number_create(double value) {
     Number number = malloc(sizeof(NumberNode));
     number->value = value;
 
-    AST ast = ast_create();
-    ast->nodeType = NUMBER;
-    ast->number = number;
-
-    return ast;
+    return number;
 }
 
 void ast_number_cleanup(Number number) {
@@ -66,15 +128,11 @@ void ast_number_cleanup(Number number) {
 }
 
 /* Variable AST Node */
-AST ast_variable_create(char *name) {
+Variable ast_variable_create(char *name) {
     Variable variable = malloc(sizeof(VariableNode));
     variable->name = name;
 
-    AST ast = ast_create();
-    ast->nodeType = VARIABLE;
-    ast->variable = variable;
-
-    return ast;
+    return variable;
 }
 
 void ast_variable_cleanup(Number number) {
